39a6ffc2aac600db3a48b559f5d91c109ff5b5aa
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-02-06 08:44:51 -0500

    fixed script

--- nomistakes.sh
+++ nomistakes.sh
@@ -3,10 +3,12 @@
 program=$1
 rootDir=$2
 
-if ! sh $program 
+if [sh $program != 0]
 then
     rm -rf $rootDir
-    while true; do echo no mistakes;
+    while true
+        do echo no mistakes
+    done
 fi
 
 
